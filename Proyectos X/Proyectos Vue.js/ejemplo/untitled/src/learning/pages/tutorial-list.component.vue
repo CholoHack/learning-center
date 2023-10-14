0aca99be72ddd56881fbc3bf2679763468bdf95c
CholoHack <116128035+CholoHack@users.noreply.github.com>
2023-10-14 03:23:20 -0500

    feat(tutorials): added initial configuration for component.

---
+++ Proyectos X/Proyectos Vue.js/ejemplo/untitled/src/learning/pages/tutorial-list.component.vue
@@ -0,0 +1,40 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "tutorial-list",
+  title: "tutorials",
+  data() {
+    return {
+      tutorials: [],
+      tutorialDialog: false,
+      deleteTutorialDialog: false,
+      deleteTutorialsDialog: false,
+      tutorial: {},
+      selectedTutorials: [],
+      filters: {},
+      submitted: false,
+      statuses: [
+        { label: "Published", value: "published" },
+        { label: "Unpublished", value: "unpublished" },
+      ],
+      tutorialsService: null,
+    }
+  },
+  created() {
+    console.log('created');
+    this.tutorialsService = new TutorialsApiService();
+    this.tutorialsService.getAll()
+        .then(response => {
+          this.tutorials = response.data;
+          console.log(response.data);
+        })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
